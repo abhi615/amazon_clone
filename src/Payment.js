46ac93d431132a75fa0fd7a04a7f3ef6bab04b66
abhijith <abhijithb626@gmail.com>
2021-07-03 11:28:32 +0530

    origin

--- src/Payment.js
+++ src/Payment.js
@@ -98,25 +98,7 @@ function Payment() {
             <p>Rajajinagar Banglore</p>
           </div>
         </div>
-
-        {/* Payment section - Review Items */}
-        <div className="payment__section">
-          <div className="payment__title">
-            <h3>Review items and delivery</h3>
-          </div>
-          <div className="payment__items">
-            {basket.map((item) => (
-              <CheckoutProduct
-                id={item.id}
-                title={item.title}
-                image={item.image}
-                price={item.price}
-                rating={item.rating}
-              />
-            ))}
-          </div>
-        </div>
-
+        
         {/* Payment section - Payment method */}
         <div className="payment__section">
           <div className="payment__title">
@@ -147,6 +129,25 @@ function Payment() {
             </form>
           </div>
         </div>
+
+        {/* Payment section - Review Items */}
+        <div className="payment__section">
+          <div className="payment__title">
+            <h3>Review items and delivery</h3>
+          </div>
+          <div className="payment__items">
+            {basket.map((item) => (
+              <CheckoutProduct
+                id={item.id}
+                title={item.title}
+                image={item.image}
+                price={item.price}
+                rating={item.rating}
+              />
+            ))}
+          </div>
+        </div>
+
       </div>
     </div>
   );
